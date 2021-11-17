56ff1571a13788f896c0cbac0c3dd362082ad206
Skyler Edvik <edvik.skyler@gmail.com>
2021-11-17 09:42:55 -0700

    Complete shopData.js util file

---
+++ src/util/shopData.js
@@ -0,0 +1,61 @@
+import bloom from '../images/bloom-doily.jpeg'
+import moonpetal from '../images/moonpetal-doily.jpg'
+import normandyLace from '../images/normandy-lace-doily.jpg'
+import paperHeart from '../images/paper-heart-doily.jpg'
+import pinkPineapple from '../images/pink-pineapple-doily.jpg'
+import royalLace from '../images/royal-lace-doily.png'
+import teaParty from '../images/tea-party-doily.jpg'
+import tulip from '../images/tulip-doily.jpg'
+
+const shopEntries = [
+  {
+    title: 'Bloom',
+    src: bloom,
+    alt: 'Bloom Doily',
+    price: 20
+  },
+  {
+    title: 'Moonpetal',
+    src: moonpetal,
+    alt: 'Moonpetal Doily',
+    price: 30
+  },
+  {
+    title: 'Normandy Lace',
+    src: normandyLace,
+    alt: 'Normandy Lace Doily',
+    price: 8
+  },
+  {
+    title: 'Paper Heart',
+    src: paperHeart,
+    alt: 'Paper Heart Doily',
+    price: 5
+  },
+  {
+    title: 'Pink Pineapple',
+    src: pinkPineapple,
+    alt: 'Pink Pineapple Doily',
+    price: 14
+  },
+  {
+    title: 'Royal Lace',
+    src: royalLace,
+    alt: 'Royal Lace Doily',
+    price: 40
+  },
+  {
+    title: 'Tea Party',
+    src: teaParty,
+    alt: 'Tea Party Doily',
+    price: 4
+  },
+  {
+    title: 'Tulip',
+    src: tulip,
+    alt: 'Tulip Doily',
+    price: 12
+  }
+]
+
+export { shopEntries }
