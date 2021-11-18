303aacf2db346c19ed78473ec5484eb862f1831b
Skyler Edvik <edvik.skyler@gmail.com>
2021-11-18 09:14:47 -0700

    Remove unused prop parameter from Home.js component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './Home.css'
 
-function Home (props) {
+function Home () {
   return (
     <div className="home-container">
       <section className="mission">
